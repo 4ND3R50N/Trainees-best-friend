578cc14d833a0e777bda7cadb8117c7ac2c9e6d5
4nd3r50n <l.pickelin@web.de>
2016-11-28 23:33:32 +0100

    Added protocol 104 + changes
    changes:
    - Changed testprotocols

--- tbfController/src/Classes/Core/ControllerCore.cs
+++ tbfController/src/Classes/Core/ControllerCore.cs
@@ -16,95 +16,142 @@ namespace tbfController.Classes.Core
     {
 
         //Variablen
-        networkServer tcpServer;
-        DBEngine databaseEngine;
-        logWriter logger;
-        List<networkServer.networkClientInterface> activeConnections;
+        networkServer TcpServer;
+        DBEngine DatabaseEngine;
+        logWriter Logger;
+        List<networkServer.networkClientInterface> ActiveConnections;
         
         private string sAesKey;
-
+        private char   cProtocolDelimiter;
 
         //Konstruktor
-        public ControllerCore(short _iPort, string _sAesKey, string _sDatabaseDriver,
+        public ControllerCore(short _iPort, char _cProtocolDelimiter, string _sAesKey, string _sDatabaseDriver,
             string _sDBHostIp, short _sDBPort, string _sDBUser, string _sDBPass, string _sDBDefaultDB, string _sLogPath)
         {
             //Logging initialisations
-            logger = new logWriter(_sLogPath);
-            logger.writeInLog(true, "Logging class initialized!");
+            Logger = new logWriter(_sLogPath);
+            Logger.writeInLog(true, "Logging class initialized!");
             //Database Initialisations
             if(_sDatabaseDriver == "mysql")
             {
-                databaseEngine = new DBMysqlManager(_sDBHostIp,_sDBUser,_sDBPass,_sDBPort,_sDBDefaultDB);
+                DatabaseEngine = new DBMysqlManager(_sDBHostIp,_sDBUser,_sDBPass,_sDBPort,_sDBDefaultDB);
 
             }else if(_sDatabaseDriver == "mssql")
             {
-                databaseEngine = new DBMssqlManager(_sDBHostIp, _sDBUser, _sDBPass, _sDBPort, _sDBDefaultDB);
+                DatabaseEngine = new DBMssqlManager(_sDBHostIp, _sDBUser, _sDBPass, _sDBPort, _sDBDefaultDB);
             }
-
-            if (databaseEngine.testDBConnection())
+            //Database test
+            if (DatabaseEngine.testDBConnection())
             {
-                logger.writeInLog(true, "Database test successfull!");
+                Logger.writeInLog(true, "Database test successfull!");
             }else
             {
-                logger.writeInLog(true, "ERROR: Database test was not successfull!");
+                Logger.writeInLog(true, "ERROR: Database test was not successfull!");
                 return;
             }
 
             //Network Initialisations
-            activeConnections = new List<networkServer.networkClientInterface>();
+            ActiveConnections = new List<networkServer.networkClientInterface>();
             sAesKey = _sAesKey;
-            tcpServer = new networkServer(networkProtocol, _sAesKey, IPAddress.Any, _iPort, AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            logger.writeInLog(true, "TCP Server ready for start!");           
+            this.cProtocolDelimiter = _cProtocolDelimiter;
+            TcpServer = new networkServer(networkProtocol, _sAesKey, IPAddress.Any, _iPort, AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Logger.writeInLog(true, "TCP Server ready for start!");
+
+
+            //TESTCASE
+            //networkServer.networkClientInterface dummy = new networkServer.networkClientInterface();
+            //networkProtocol("#104;Anderson2;Lars;Pickelin;miau1234;l.pickelin@web.de", ref dummy);
+                       
         }
 
         public void start()
         {
-            if(tcpServer.startListening())
+            if(TcpServer.startListening())
             {
-                logger.writeInLog(true, "Server has been started successfully!");
+                Logger.writeInLog(true, "Server has been started successfully!");
             }
             else
             {
-
-                logger.writeInLog(true, "ERROR: The server was not able to start!");
+                Logger.writeInLog(true, "ERROR: The server was not able to start!");
             }
            
         }
         
         private void networkProtocol(string message, ref networkServer.networkClientInterface relatedClient)
         {
-            string protocolShortcut = getProtocolShortcut(message);
-            string realMessage = getProtocolMessage(message);
-            Console.WriteLine("["+ DateTime.Now + "] system@tbf-controller: Data revceived: " + message);
-            switch (protocolShortcut)
+            string sProtocolShortcut = getProtocolShortcut(message);
+            // Put data in array
+            List<string> lDataList = new List<string>();
+            lDataList = getProtocolData(getProtocolMessage(message));
+
+            switch (sProtocolShortcut)
             {
-                case "#101":
-                    Console.WriteLine("[" + DateTime.Now + "] system@tbf-controller: Got #101, send answer back!");
-                    string test = "";
-                    for (int i = 0; i < 20; i++)
-                    {
-                        test = test + "Daten Packet. Xamarin + Controller + Content manager = Cool!---------------------------------------------------------------------------------fhgfchfhffhfvfgvchfhvhfhvfhvfhvhvfhfzzree5e5frzrfzrzfrzrfzrtzrzrzrzrzrzzrzrvrtreteswrwr\n";
-                    }
-                    tcpServer.sendMessage(test, relatedClient);
-                    break;
+                case "#001":
+                    prot_001_TestPackage(ref relatedClient); break;
+                case "#003":
+                    prot_003_TestPackage(lDataList, ref relatedClient); break;
+
+                case "#104":
+                    prot_104_registerUser(lDataList, ref relatedClient); break;
+
                 default:
-                    Console.WriteLine(message);
+                    Logger.writeInLog(true, "Unknown package protocol/data received: " + message);
                     break;
             }
         }
 
-        #region Support functions
-        private string getProtocolShortcut(string message)
+        #region Protocol functions
+        //Testpackages
+        private void prot_001_TestPackage(ref networkServer.networkClientInterface relatedClient)
         {
-            return message.Substring(0, 4);
+            Logger.writeInLog(true, "Message #001 (TESTPACKET_NORMAL) received from a client!");
+            TcpServer.sendMessage("#002;Greetings from Controller :)", relatedClient);
+            Logger.writeInLog(true, "Answered #002 with the greetings message!");
         }
-        private string getProtocolMessage(string message)
+        private void prot_003_TestPackage(List<string> lDataList, ref networkServer.networkClientInterface relatedClient)
         {
-            return message.Substring(4);
+            Logger.writeInLog(true, "Message #003 (TESTPACKET_LARGE) received from a client!");
+            string sDataPackage = "#004;";
+            int iForCounter = 40;
+            for (int i = 0; i < iForCounter; i++)
+            {
+                sDataPackage = sDataPackage + "Package number: " + (i + 1) + ". This is a large datapackage to test the network tcp/ip receive function. You should get " + iForCounter + " of these packages! Good luck, programmer!\n";
+            }
+            TcpServer.sendMessage(sDataPackage, relatedClient);
+            Logger.writeInLog(true, "Answered #004 with the large datapacket!");
+        }
+        //login
+
+        //Signup
+        private void prot_104_registerUser(List<string> lDataList, ref networkServer.networkClientInterface relatedClient)
+        {
+            //log
+            Logger.writeInLog(true, "Message #104 (SIGNUP) received from a client!");
+            //register user
+            int iSignUpStatusCode = DatabaseEngine.signUpRegisterUser(lDataList[0], lDataList[1], lDataList[2], lDataList[3], lDataList[4]);
+            //send message to client
+            TcpServer.sendMessage("#105" + cProtocolDelimiter + iSignUpStatusCode, relatedClient);
+            Logger.writeInLog(true, "Answered #105 with SignUpCode "+ iSignUpStatusCode + "!");
         }
         #endregion
 
 
+        #region Support functions
+        private List<string> getProtocolData(string message)
+        {
+            return message.Split(cProtocolDelimiter).ToList();
+        }
 
+        private string getProtocolShortcut(string message)
+        {
+            return message.Split(cProtocolDelimiter)[0];
+        }
+        private string getProtocolMessage(string message)
+        {
+            Console.WriteLine(message.Substring(getProtocolShortcut(message).Length));
+            return message.Substring(getProtocolShortcut(message).Length + 1);
+        }
+        #endregion
+        
     }
 }
