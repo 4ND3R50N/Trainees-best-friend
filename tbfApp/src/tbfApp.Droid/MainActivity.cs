56f0c15846e2d25bf377dbbb636d43e8efd8ad2a
techniko27 <nikoscheuermann@web.de>
2017-06-15 13:02:15 +0200

    Octane Xamarin Video Player
    tested external video player

--- tbfApp/src/tbfApp.Droid/MainActivity.cs
+++ tbfApp/src/tbfApp.Droid/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Octane.Xam.VideoPlayer.Android;
 
 namespace tbfApp.Droid
 {
@@ -20,6 +21,10 @@ namespace tbfApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            //Initialize Xamarin Forms Video Player
+            //FormsVideoPlayer.Init();
+
             LoadApplication(new App());
         }
     }
