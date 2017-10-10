6d85963337afa15fe955133523ee625a333867a4
techniko27 <nikoscheuermann@web.de>
2017-10-10 17:26:24 +0200

    Upload Class and ProgressBar new implemented

---
+++ tbfContentManager/src/Classes/Upload.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using MahApps.Metro.Controls;
+using Renci.SshNet;
+using MessageBox = System.Windows.MessageBox;
+
+
+namespace tbfContentManager.Classes
+{
+    public class Upload
+    {
+        static MessageWindow msgWin;
+
+        public static void UploadSFTPFile(Window owner, string host, string username,
+        string password, string sourcefile, string destinationpath, int port)
+        {
+            // Overlay
+            msgWin = new MessageWindow(owner, true);
+            msgWin.Title = "Upload";
+            msgWin.UpperText = "Upload in progress.";
+            msgWin.LowerText = "Please wait...";
+            msgWin.pb1.Visibility = Visibility.Visible;
+
+            msgWin.Show();
+
+            // Run Upload on background thread
+            //Task.Run(() => UploadInOtherThread(host, username, password, sourcefile, destinationpath, port));
+            
+        }
+
+        static private void UploadInOtherThread(string host, string username,
+        string password, string sourcefile, string destinationpath, int port)
+        {
+            using (SftpClient client = new SftpClient(host, port, username, password))
+            {
+                client.Connect();
+                client.ChangeDirectory(destinationpath);
+
+                using (FileStream fs = new FileStream(sourcefile, FileMode.Open))
+                {
+                    client.BufferSize = 4 * 1024;
+
+                    // Set progress bar maximum on foreground thread
+                    msgWin.pb1.Invoke(delegate { msgWin.pb1.Maximum = (int)fs.Length; });
+
+                    client.UploadFile(fs, Path.GetFileName(sourcefile), UpdateProgresBar);
+                }
+                client.Disconnect();
+            }
+
+            msgWin.Invoke(delegate { msgWin.preventClose = false; });
+            msgWin.Invoke(delegate { msgWin.Close(); } );
+
+            MessageBox.Show("Die Datei wurde erfolgreich hochgeladen!", "Hochgeladen", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        static private void UpdateProgresBar(ulong uploaded)
+        {
+            // Update progress bar on foreground thread
+            msgWin.pb1.Invoke(delegate { msgWin.pb1.Value = (int)uploaded; });
+        }
+    }
+}
---
+++ tbfContentManager/src/MessageWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tbfContentManager
+{
+    /// <summary>
+    /// Interaktionslogik für MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow
+    {
+        bool pb1Enabled;
+        public bool preventClose;
+
+        public MessageWindow(Window owner, bool preventClose = false)
+        {
+            pb1Enabled = false;
+            this.preventClose = preventClose;
+            Owner = owner;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            InitializeComponent();
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(preventClose)
+                e.Cancel = true;
+        }
+
+        public string UpperText
+        {
+            get
+            {
+                return (string) labelUpper.Content;
+            }
+            set
+            {
+                if(labelUpper.Content == null)
+                {
+                    Height += 75;
+                    labelUpper.Visibility = Visibility.Visible;
+                }
+                labelUpper.Content = value;
+            }
+        }
+        public string LowerText
+        {
+            get
+            {
+                return (string)labelLower.Content;
+            }
+            set
+            {
+                if (labelLower.Content == null)
+                {
+                    Height += 75;
+                    labelLower.Visibility = Visibility.Visible;
+                }
+                labelLower.Content = value;
+            }
+        }
+        /*
+        public double Progress
+        {
+            get
+            {
+                return pb1.Value;
+            }
+            set
+            {
+                if (!pb1Enabled)
+                {
+                    pb1Enabled = true;
+                    Height += 25;
+                    pb1.Visibility = Visibility.Visible;
+                }
+                pb1.Value = value;
+            }
+        }
+        */
+    }
+}
